b491180815ad75e2d5ca088122ce478b3316ce46
FranciscoRivera2 <71896003+FranciscoRivera2@users.noreply.github.com>
2020-11-21 18:55:34 -0300

    septimo

--- info tech/info tech/almacenamiento.Designer.cs
+++ info tech/info tech/almacenamiento.Designer.cs
@@ -29,19 +29,113 @@ namespace info_tech
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(almacenamiento));
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.lblTipo1 = new System.Windows.Forms.Label();
+            this.lblTipo2 = new System.Windows.Forms.Label();
+            this.lblAlmacenamiento1 = new System.Windows.Forms.Label();
+            this.lblAlmacenamiento2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1308, 60);
+            this.label1.TabIndex = 0;
+            this.label1.Text = resources.GetString("label1.Text");
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Seagate barracuda",
+            "WD green"});
+            this.comboBox1.Location = new System.Drawing.Point(71, 201);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Seagate barracuda",
+            "WD green"});
+            this.comboBox2.Location = new System.Drawing.Point(547, 201);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 2;
+            // 
+            // lblTipo1
+            // 
+            this.lblTipo1.AutoSize = true;
+            this.lblTipo1.Location = new System.Drawing.Point(98, 272);
+            this.lblTipo1.Name = "lblTipo1";
+            this.lblTipo1.Size = new System.Drawing.Size(31, 13);
+            this.lblTipo1.TabIndex = 3;
+            this.lblTipo1.Text = "Tipo:";
+            // 
+            // lblTipo2
+            // 
+            this.lblTipo2.AutoSize = true;
+            this.lblTipo2.Location = new System.Drawing.Point(544, 272);
+            this.lblTipo2.Name = "lblTipo2";
+            this.lblTipo2.Size = new System.Drawing.Size(31, 13);
+            this.lblTipo2.TabIndex = 4;
+            this.lblTipo2.Text = "Tipo:";
+            // 
+            // lblAlmacenamiento1
+            // 
+            this.lblAlmacenamiento1.AutoSize = true;
+            this.lblAlmacenamiento1.Location = new System.Drawing.Point(98, 341);
+            this.lblAlmacenamiento1.Name = "lblAlmacenamiento1";
+            this.lblAlmacenamiento1.Size = new System.Drawing.Size(88, 13);
+            this.lblAlmacenamiento1.TabIndex = 5;
+            this.lblAlmacenamiento1.Text = "Almacenamiento:";
+            // 
+            // lblAlmacenamiento2
+            // 
+            this.lblAlmacenamiento2.AutoSize = true;
+            this.lblAlmacenamiento2.Location = new System.Drawing.Point(544, 341);
+            this.lblAlmacenamiento2.Name = "lblAlmacenamiento2";
+            this.lblAlmacenamiento2.Size = new System.Drawing.Size(88, 13);
+            this.lblAlmacenamiento2.TabIndex = 6;
+            this.lblAlmacenamiento2.Text = "Almacenamiento:";
             // 
             // almacenamiento
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1116, 450);
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.lblAlmacenamiento2);
+            this.Controls.Add(this.lblAlmacenamiento1);
+            this.Controls.Add(this.lblTipo2);
+            this.Controls.Add(this.lblTipo1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
             this.Name = "almacenamiento";
             this.Text = "Form3";
+            this.Load += new System.EventHandler(this.almacenamiento_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label lblTipo1;
+        private System.Windows.Forms.Label lblTipo2;
+        private System.Windows.Forms.Label lblAlmacenamiento1;
+        private System.Windows.Forms.Label lblAlmacenamiento2;
     }
 }
--- info tech/info tech/almacenamiento.cs
+++ info tech/info tech/almacenamiento.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.OleDb;
 
 namespace info_tech
 {
@@ -15,6 +16,171 @@ namespace info_tech
         public almacenamiento()
         {
             InitializeComponent();
+        }
+
+        private void almacenamiento_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Tipo();
+        }
+        // creacion de un method 
+        private void Tipo()
+        {
+            if (comboBox1.Text == "Seagate barracuda")
+            {
+                //coneccion con la basedata
+                OleDbConnection con = new OleDbConnection(@"Provider = Microsoft.ACE.OLEDB.12.0; Data Source = nuevainfotech.accdb");
+                //abrimos coneccion
+                con.Open();
+                // comando de lo que va a ser "SELECT nombre de campo FROM nombre de entidad WHERE Id = ...", con
+                OleDbCommand cmd = new OleDbCommand("SELECT Tipo FROM Almacenamiento1 WHERE Id = 1", con);
+                //lee lo que ejecuto el cmd
+                OleDbDataReader rd = cmd.ExecuteReader();
+                rd.Read();
+                // hasta aca 
+                //en una variable string guarda lo que saco de la base de datos
+                string canTipo = rd.GetString(0);
+                // aca abajo iria el label en donde se imprime el string previamente dicho
+                lblTipo1.Text = "Tipo:" + canTipo;
+
+
+            }
+            else if (comboBox1.Text == "WD green")
+            {
+                //coneccion con la basedata
+                OleDbConnection con = new OleDbConnection(@"Provider = Microsoft.ACE.OLEDB.12.0; Data Source = nuevainfotech.accdb");
+                //abrimos coneccion
+                con.Open();
+                // comando de lo que va a ser "SELECT nombre de campo FROM nombre de entidad WHERE Id = ...", con
+                OleDbCommand cmd = new OleDbCommand("SELECT Tipo FROM Almacenamiento1 WHERE Id = 2", con);
+                //lee lo que ejecuto el cmd
+                OleDbDataReader rd = cmd.ExecuteReader();
+                rd.Read();
+                // hasta aca 
+                //en una variable string guarda lo que saco de la base de datos
+                string canTipo = rd.GetString(0);
+                // aca abajo iria el label en donde se imprime el string previamente dicho
+                lblTipo1.Text = "Tipo:" + canTipo;
+
+
+            }
+            if (comboBox2.Text == "Seagate barracuda")
+            {
+                //coneccion con la basedata
+                OleDbConnection con = new OleDbConnection(@"Provider = Microsoft.ACE.OLEDB.12.0; Data Source = nuevainfotech.accdb");
+                //abrimos coneccion
+                con.Open();
+                // comando de lo que va a ser "SELECT nombre de campo FROM nombre de entidad WHERE Id = ...", con
+                OleDbCommand cmd = new OleDbCommand("SELECT Tipo FROM Almacenamiento1 WHERE Id = 1", con);
+                //lee lo que ejecuto el cmd
+                OleDbDataReader rd = cmd.ExecuteReader();
+                rd.Read();
+                // hasta aca 
+                //en una variable string guarda lo que saco de la base de datos
+                string canTipo = rd.GetString(0);
+                // aca abajo iria el label en donde se imprime el string previamente dicho
+                lblTipo2.Text = "Tipo:" + canTipo;
+
+
+            }
+            else if (comboBox2.Text == "WD green")
+            {
+                //coneccion con la basedata
+                OleDbConnection con = new OleDbConnection(@"Provider = Microsoft.ACE.OLEDB.12.0; Data Source = nuevainfotech.accdb");
+                //abrimos coneccion
+                con.Open();
+                // comando de lo que va a ser "SELECT nombre de campo FROM nombre de entidad WHERE Id = ...", con
+                OleDbCommand cmd = new OleDbCommand("SELECT Tipo FROM Almacenamiento1 WHERE Id = 2", con);
+                //lee lo que ejecuto el cmd
+                OleDbDataReader rd = cmd.ExecuteReader();
+                rd.Read();
+                // hasta aca 
+                //en una variable string guarda lo que saco de la base de datos
+                string canTipo = rd.GetString(0);
+                // aca abajo iria el label en donde se imprime el string previamente dicho
+                lblTipo2.Text = "Tipo:" + canTipo;
+
+
+            }
+        }
+        private void Almacenamiento()
+        {
+            if (comboBox1.Text == "Seagate barracuda")
+            {
+                //coneccion con la basedata
+                OleDbConnection con = new OleDbConnection(@"Provider = Microsoft.ACE.OLEDB.12.0; Data Source = nuevainfotech.accdb");
+                //abrimos coneccion
+                con.Open();
+                // comando de lo que va a ser "SELECT nombre de campo FROM nombre de entidad WHERE Id = ...", con
+                OleDbCommand cmd = new OleDbCommand("SELECT Almacenamiento FROM Almacenamiento1 WHERE Id = 1", con);
+                //lee lo que ejecuto el cmd
+                OleDbDataReader rd = cmd.ExecuteReader();
+                rd.Read();
+                // hasta aca 
+                //en una variable string guarda lo que saco de la base de datos
+                string canAlmacenamiento = rd.GetString(0);
+                // aca abajo iria el label en donde se imprime el string previamente dicho
+                lblAlmacenamiento1.Text = "Almacenamiento:" + canAlmacenamiento;
+
+            }
+            else if (comboBox1.Text == "WD green")
+            {
+                //coneccion con la basedata
+                OleDbConnection con = new OleDbConnection(@"Provider = Microsoft.ACE.OLEDB.12.0; Data Source = nuevainfotech.accdb");
+                //abrimos coneccion
+                con.Open();
+                // comando de lo que va a ser "SELECT nombre de campo FROM nombre de entidad WHERE Id = ...", con
+                OleDbCommand cmd = new OleDbCommand("SELECT Almacenamiento FROM Almacenamiento1 WHERE Id = 2", con);
+                //lee lo que ejecuto el cmd
+                OleDbDataReader rd = cmd.ExecuteReader();
+                rd.Read();
+                // hasta aca 
+                //en una variable string guarda lo que saco de la base de datos
+                string canAlmacenamiento = rd.GetString(0);
+                // aca abajo iria el label en donde se imprime el string previamente dicho
+                lblAlmacenamiento1.Text = "Almacenamiento:" + canAlmacenamiento;
+            }
+            if (comboBox2.Text == "Seagate barracuda")
+            {
+                //coneccion con la basedata
+                OleDbConnection con = new OleDbConnection(@"Provider = Microsoft.ACE.OLEDB.12.0; Data Source = nuevainfotech.accdb");
+                //abrimos coneccion
+                con.Open();
+                // comando de lo que va a ser "SELECT nombre de campo FROM nombre de entidad WHERE Id = ...", con
+                OleDbCommand cmd = new OleDbCommand("SELECT Almacenamiento FROM Almacenamiento1 WHERE Id = 1", con);
+                //lee lo que ejecuto el cmd
+                OleDbDataReader rd = cmd.ExecuteReader();
+                rd.Read();
+                // hasta aca 
+                //en una variable string guarda lo que saco de la base de datos
+                string canAlmacenamiento = rd.GetString(0);
+                // aca abajo iria el label en donde se imprime el string previamente dicho
+                lblAlmacenamiento2.Text = "Almacenamiento:" + canAlmacenamiento;
+
+
+            }
+            else if (comboBox2.Text == "WD green")
+            {
+                //coneccion con la basedata
+                OleDbConnection con = new OleDbConnection(@"Provider = Microsoft.ACE.OLEDB.12.0; Data Source = nuevainfotech.accdb");
+                //abrimos coneccion
+                con.Open();
+                // comando de lo que va a ser "SELECT nombre de campo FROM nombre de entidad WHERE Id = ...", con
+                OleDbCommand cmd = new OleDbCommand("SELECT Almacenamiento FROM Almacenamiento1 WHERE Id = 2", con);
+                //lee lo que ejecuto el cmd
+                OleDbDataReader rd = cmd.ExecuteReader();
+                rd.Read();
+                // hasta aca 
+                //en una variable string guarda lo que saco de la base de datos
+                string canAlmacenamiento = rd.GetString(0);
+                // aca abajo iria el label en donde se imprime el string previamente dicho
+                lblAlmacenamiento2.Text = "Almacenamiento:" + canAlmacenamiento;
+            }
+
         }
     }
 }
